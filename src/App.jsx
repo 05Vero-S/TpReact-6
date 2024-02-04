98b01722e4915b48325b7a446dc8c959733750f9
Vero <vdsoria05@gmail.com>
2024-02-04 17:58:08 -0300

    agregue maquetado del componente FormularioColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
+import { Container } from 'react-bootstrap'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import FormularioColor from './components/FormularioColor'
 
 function App() {
   
 
   return (
-    <>
-      
-       
-    </>
+    <Container className='my-4'>
+      <h1 className='text-center my-3'>Paleta de Colores</h1>
+      <FormularioColor/>
+
+    </Container>
   )
 }
 
---
+++ src/components/FormularioColor.jsx
@@ -0,0 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
+const FormularioColor = () => {
+    return (
+        <section>
+            <Card >
+      <Card.Header>Administrar colores</Card.Header>
+      <Card.Body className='bg-secondary'>
+      <Form >
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label >Color</Form.Label>
+        <Form.Control type="text" placeholder="Ingrese un color ej. Blue" />
+      </Form.Group>
+      
+    </Form>
+    
+      </Card.Body>
+      <Button variant="info" type='submit'>Guardar</Button>
+    </Card>
+            
+        </section>
+    );
+};
+
+export default FormularioColor;
