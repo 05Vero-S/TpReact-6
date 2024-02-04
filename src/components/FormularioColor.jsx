099590c294ddfd0451e2b653606d4d9532e201a0
Vero <vdsoria05@gmail.com>
2024-02-04 18:48:40 -0300

    agregue el state color cuando agregamos un color en el formulario

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,29 +1,37 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import ListaColores from './ListaColores';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import ListaColores from "./ListaColores";
+import { useState } from "react";
 
 const FormularioColor = () => {
-    return (
-        <section>
-            <Card className='my-4'>
-      <Card.Header>Administrar colores</Card.Header>
-      <Card.Body className='bg-secondary'>
-      <Form >
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label >Color</Form.Label>
-        <Form.Control type="text" placeholder="Ingrese un color ej. Blue" minLength={2} maxLength={20}/>
-      </Form.Group>
-      
-    </Form>
-    
-      </Card.Body>
-      <Button variant="info" type='submit'>Guardar</Button>
-    </Card>
-    <ListaColores/>
-            
-        </section>
-    );
+  const [color, setColor] = useState("");
+  return (
+    <section>
+      <Card className="my-4">
+        <Card.Header>Administrar colores</Card.Header>
+        <Card.Body className="bg-secondary">
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Color</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un color ej. Blue"
+                minLength={2}
+                maxLength={20}
+                onChange={(e)=> setColor(e.target.value)}
+                value={color}
+              />
+            </Form.Group>
+          </Form>
+        </Card.Body>
+        <Button variant="info" type="submit">
+          Guardar
+        </Button>
+      </Card>
+      <ListaColores />
+    </section>
+  );
 };
 
 export default FormularioColor;
